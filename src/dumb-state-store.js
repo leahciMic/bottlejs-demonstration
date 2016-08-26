57d857800a71f07d5e16f813d456d94ae4518c19
Michael Leaney <mleaney@tokbox.com>
2016-08-26 10:30:35 +1000

    Clean up

--- src/dumb-state-store.js
+++ src/dumb-state-store.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-module.exports = function keyValueStore(container) {
+module.exports = function keyValueStore({ STATE_STORE_FILE_URL }) {
   function readStorage() {
     try {
-      return JSON.parse(fs.readFileSync(container.STATE_STORE_FILE_URL, 'utf8'));
+      return JSON.parse(fs.readFileSync(STATE_STORE_FILE_URL, 'utf8'));
     } catch(err) {
       if (err.code === 'ENOENT') { return {}; }
       throw err;
@@ -13,7 +13,7 @@ module.exports = function keyValueStore(container) {
   this.set = (key, value) => {
     const data = readStorage();
     data[key] = value;
-    fs.writeFileSync(container.STATE_STORE_FILE_URL, JSON.stringify(data), 'utf8');
+    fs.writeFileSync(STATE_STORE_FILE_URL, JSON.stringify(data), 'utf8');
   };
 
   this.get = key => readStorage()[key];
